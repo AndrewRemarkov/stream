ac4032df9e689f0d9d3b07a5b3005f518e5e38f8
RemarkoWeb <remarkoweb@gmail.com>
2025-04-10 03:39:14 +0700

    first commit

---
+++ src/components/Socials/Socials.jsx
@@ -0,0 +1,32 @@
+import './Socials.scss'
+import classNames from "classnames";
+import Button from "@/components/Button";
+
+const Socials = (props) => {
+  const {className, links = [],} = props
+
+  return (
+    <div
+      className={classNames(className, 'soc1als')}
+    >
+      <ul className="soc1als__list">
+        {links.map(({label, iconName}, i) => (
+          <li className={'soc1als__item'} key={i}>
+            <Button
+              className="soc1als__link"
+              href={'/'}
+              target="_blank"
+              mode={'black-10'}
+              label={label}
+              isLabelHidden
+              iconName={iconName}
+              hasFillIcon
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Socials
